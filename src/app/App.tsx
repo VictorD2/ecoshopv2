e44f899517e4561659629e6db67ed2f6505a5891
Victor Hernandez <victor-2027@hotmail.com>
2021-11-04 15:26:23 -0500

    Plantilla

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import { UsuarioProvider } from "../auth/UsuarioProvider";
 
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import NotFound from "../pages/NotFound";
 
-// Layout
-import LayoutDash from "../partials/LayoutDash";
-import LayoutUsuario from "../partials/LayoutUsuario";
-
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Vistas */}
-        {/* <LayoutDash element={<Login />} path="/" /> */}
-        {/* <LayoutUsuario element={<Login />} path="/" /> */}
         <Route element={<Login />} path="/Iniciar" />
         <Route element={<Register />} path="/Registrarse" />
         {/* Dashboard */}
